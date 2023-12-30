2b823ada0499fe8ae7735b4bc331da4c9f60dd2f
Godruoyi <godruoyi@gmail.com>
2023-12-30 17:53:57 +0800

    feat: disable weather since it always timeout

--- services/hydrate_service.py
+++ services/hydrate_service.py
@@ -9,4 +9,7 @@ class HydrateService:
 
     @staticmethod
     def get_weather(city: str):
+        # disable fill weather info when city is not set
+        if city is None or city == "":
+            return ""
         return get_weather2(city)
